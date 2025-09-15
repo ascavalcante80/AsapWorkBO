bd9db544362f7e3b86bfbc36b7f45b5204903924
alexandre <ascavalcante80@gmail.com>
2025-09-15 12:11:50 +0200

    set kDebugMode

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // use emulator
-  if (false) {
+  if (kDebugMode) {
     FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
     FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
   }
